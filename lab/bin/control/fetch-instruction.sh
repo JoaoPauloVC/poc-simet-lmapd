ef396d070387147123ad51a9bbc6c4e01204c9c9
JoaoPauloVC <joaop.carvalho01@gmail.com>
2026-09-10 12:45:25 -0300

    Updated to initial temp_state when boot

--- lab/bin/control/fetch-instruction.sh
+++ lab/bin/control/fetch-instruction.sh
@@ -8,31 +8,71 @@ LAB_DIR="$(cd "${PROJECT_DIR}/lab" && pwd)"
 
 LMAPCTL="${PROJECT_DIR}/build/src/lmapctl"
 
-INSTRUCTION_URL="http://localhost:4000/v1/agents/test-ma/required-config"
+AGENT_ID="test-ma"
+CONTROLLER_URL="http://localhost:4000/v1/agents/${AGENT_ID}"
+
+STATE_URL="${CONTROLLER_URL}/reported-state"
+INSTRUCTION_URL="${CONTROLLER_URL}/required-config"
+
+CAPABILITIES="${LAB_DIR}/capabilities"
 CURRENT_INSTRUCTION="${LAB_DIR}/config/config.json"
+QUEUE="${LAB_DIR}/queue"
+RUN="${LAB_DIR}/run"
+
+TEMPORARY_STATE="$(mktemp "${LAB_DIR}/config/.reported-state.XXXXXX")"
 TEMPORARY_INSTRUCTION="$(mktemp "${LAB_DIR}/config/.instruction.XXXXXX")"
 
+# Cleanup temporary files on exit
 cleanup() {
+  rm -f "${TEMPORARY_STATE}"
   rm -f "${TEMPORARY_INSTRUCTION}"
 }
+
 trap cleanup EXIT
 
+# Generate current LMAP state
+"${PROJECT_DIR}/build/src/lmapd" \
+  -j \
+  -s \
+  -b "${CAPABILITIES}" \
+  -c "${CURRENT_INSTRUCTION}" \
+  -q "${QUEUE}" \
+  -r "${RUN}" \
+  > "${TEMPORARY_STATE}" || true
+
+# Validate generated state JSON
+jq empty "${TEMPORARY_STATE}"
+
+# Report current state to Controller
+curl --fail --silent --show-error \
+  -X PUT \
+  -H "Content-Type: application/json" \
+  --data-binary @"${TEMPORARY_STATE}" \
+  "${STATE_URL}"
+
+# GET desired Instruction from Controller
 curl --fail --silent --show-error \
   --output "${TEMPORARY_INSTRUCTION}" \
   "${INSTRUCTION_URL}"
 
+# Validate received JSON
+jq empty "${TEMPORARY_INSTRUCTION}"
+
+# Validate received LMAP Instruction
 "${LMAPCTL}" \
   -j \
-  -q "${LAB_DIR}/queue" \
-  -r "${LAB_DIR}/run" \
+  -q "${QUEUE}" \
+  -r "${RUN}" \
   -c "${TEMPORARY_INSTRUCTION}" \
   validate
 
+# Replace current Instruction
 mv "${TEMPORARY_INSTRUCTION}" "${CURRENT_INSTRUCTION}"
 
+# Reload lmapd
 "${LMAPCTL}" \
   -j \
-  -q "${LAB_DIR}/queue" \
-  -r "${LAB_DIR}/run" \
+  -q "${QUEUE}" \
+  -r "${RUN}" \
   -c "${CURRENT_INSTRUCTION}" \
   reload
